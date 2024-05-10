86c83214cad2c1a9b9fb08e8837e818c40c18c32
Caroline Spezzatto <caroline.spezzatto@paketa.com.br>
2024-05-09 21:13:09 -0300

    uri changed

--- app/src/providers/apollo.ts
+++ app/src/providers/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://bookshelf.vercel.app',
+  uri: 'https://bookshelf-app-tau.vercel.app/',
   // uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
   name: 'web-books',
